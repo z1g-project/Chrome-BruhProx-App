ff4dcc7cc4ec268dfd126581d308dd8a02c7498e
Notplayingallday383 <72810050+Notplayingallday383@users.noreply.github.com>
2023-01-12 10:42:00 -0500

    Added Source Code Files

---
+++ source/config.js
@@ -0,0 +1,10 @@
+window.config = {
+   "model_": "AppConfig",
+   "id": 1,
+   "appName": "BruhProx",
+   "version": "5.4.0",
+   "homepage": "https://bruhprox.johnglynn2.repl.co/",
+   "enableLogoutBttn": false,
+   "termsOfService": "",
+   "kioskEnabled": true
+};
